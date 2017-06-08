dfed0dbd56bf110361a3a32f1d3f5a497cf36e05
name <email@email.com>
2017-06-08 13:48:30 +0800

    1

--- stun_client.js
+++ stun_client.js
@@ -1,6 +1,6 @@
 var stun = require('node-stun');
 var client = stun.createClient();
-client.setServerAddr('stun.ideasip.com', 80);
+client.setServerAddr('120.27.32.78', 9002);
 client.start({}, function() {
     console.log(client.getMappedAddr())
 });
