de7da66cf7153597c3a65037f6f4f3b7ce9045a0
name <email@email.com>
2017-06-08 14:14:17 +0800

    1

--- stun_client.js
+++ stun_client.js
@@ -1,6 +1,9 @@
 var stun = require('node-stun');
 var client = stun.createClient();
-client.setServerAddr('120.27.32.78', 9002);
+//client.setServerAddr('120.27.32.78', 29695);
+client.setServerAddr('120.27.32.78', 3478);
 client.start({}, function() {
     console.log(client.getMappedAddr())
 });
+
+//  iptables -I INPUT -p tcp --dport 3478 -j ACCEPT
