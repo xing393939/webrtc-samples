bc971a602a7dea4c5b461a9b449ef4ee6261415a
name <email@email.com>
2017-06-15 09:30:24 +0800

    ssl

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 var app = express();
-var server = app.listen(9003);
+var server = require('https').createServer({
+    key: fs.readFileSync('./cert/server.key', 'utf8'),
+    cert: fs.readFileSync('./cert/server.crt', 'utf8')
+}, app);
+server.listen(9003);
 app.use('/api', ExpressPeerServer(server, {debug: true}));
 
 app.use(express.static('./static'));
